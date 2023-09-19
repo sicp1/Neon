0cea8d93c4bcb101273cdfeef2c7188edb1ca31d
sicp1 <107533664+sicp1@users.noreply.github.com>
2023-09-19 19:51:30 +0800

    Update user.py

--- user.py
+++ user.py
@@ -19,26 +19,22 @@ from HTMLSocket import HTMLSocket
 from MySqlData.mySqlFunc import ShowMySqlDataBases ,GetTableAllData
 from ExcelData.excelBasic import GetAllData
 def init():#初始化
-    basicData.GetExcel_Sheet("./static/FirstData.xlsx","回收订单明细","firstdata")
-    basicData.SelectNowExcel("firstdata")
-    mainData.MainData()
+    basicData.GetExcel("./static/fujian.xlsx","babysleep")
+    basicData.SelectNowExcel("babysleep")
+
+    mainData.MainData()#不要动这个
 init()
 ###用户编写区域############
 async def start(websocket):
-
     print("-----start-----")
-    await mainFunc.NormalCard(websocket,BarChart("bar1",myData["firstdata_7_8_name"],myData['firstdata_7_8_data'],"型号与报单数量")+
-                              BarChart("bar2",myData['firstdata_7_12_name'],myData['firstdata_7_12_data'],"型号与收单数量")+
-                              BarChart("bar3",myData['firstdata_7_name'],myData['firstdata_8_data'],"型号与报单数量"),"数据分析")
-    # await mainFunc.NormalCard(websocket,mainFunc.BeautifulInput("x1","请在这里输入x1")+
-    #
-    # mainFunc.BeautifulInput("x2","请在这里输入x2")+
-    # mainFunc.NextLine()
-    # +mainFunc.BeautifulButton("计算","GenShenStart","""["x1","x2"]""","y1")
-    # +mainFunc.NextLine()
-    # +mainFunc.BeautifulOutPut("y1","输出")
-    #                           ,"结果")LineChart("firstLove",myData['sztu1'],myData['sztu2'], "excel1")+
-    #                               LineChart("twiceLove",myData['863_1'],myData['863_2'],"excel2")+
+    await mainFunc.NormalCard(websocket,mainFunc.BeautifulInput("x1","请在这里输入x1")+
+    mainFunc.BeautifulInput("x2","请在这里输入x2")+
+    mainFunc.NextLine()
+    +mainFunc.BeautifulButton("计算","GenShenStart","""["x1","x2"]""","y1")
+    +mainFunc.NextLine()
+    +mainFunc.BeautifulOutPut("y1","输出"),"结果")
+
+    await mainFunc.NormalCard(websocket,LineChart("baby_mother",myData['babyType'],myData['motherAge'],"宝宝类型和妈妈年龄"),"第二个卡片")
     # await mainFunc.NormalCard(websocket, mainFunc.BeautifulInput("x1", "请在这里输入x1") +
     #                           mainFunc.BeautifulInput("x2","请在这里输入x2")+
     #                            mainFunc.NextLine()
@@ -59,5 +55,7 @@ Thread_HTMLSocket.start()
 start_server = websockets.serve(Main, "127.0.0.1", 8888)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
+
 
 
